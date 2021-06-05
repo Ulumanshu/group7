7a53058239876f4d6719712eee4e9abcc3139a55
Ulumanshu <karolis_kesminas@yahoo.com>
2021-06-05 11:20:22 +0300

    Add variables with complex types, list

--- data_types.py
+++ data_types.py
@@ -5,6 +5,8 @@ integer1 = 1
 integer2 = 2
 integer3 = 3
 
+# print('Visi sveiki skaiciai: ', integer1, integer2, integer3)
+
 # Float - Skaicius su kableliu
 float1 = 1.53
 float2 = 2.45
@@ -22,6 +24,21 @@ bool2 = False
 # None - tipas neapibreztiems dalykams
 # TODO paaiskinti prasme funcijos priemimo skliaustuose, pvz su sarasu def kazkas(kint1=None arba [])!
 none1 = None
+
+# List (Array) - sarasas, su pozicijomis ir pirma pozicija yra 0
+list1 = list()  # list1 = []
+list2 = []
+list3 = [bool1, bool2]
+
+print("Visi sarasai po sukurimo", list1, list2, list3)
+# Saraso valdymo bazines komandos
+list1.append('tiesiog reiksme')  # galima prideti duomenis tiesiogiai
+list1.append(integer1)  # galima prideti kintamaji, prideti elementai visad prisideda gale
+list2.append(string2)
+list2.append(float2)
+print("Visi sarasai po pridejimo", list1, list2, list3)
+
+
 
 
 
