de04281773237d620a0dac5360c4b63b30f5007f
Ulumanshu <karolis_kesminas@yahoo.com>
2021-06-12 10:39:58 +0300

    Add more comparison operators

---
+++ identity_membership_logical_operators.py
@@ -0,0 +1,19 @@
+# Identity operators 	is, is not
+# Logical operators 	and, or, not
+# Membership operators 	in, not in
+a = list()
+b = list()
+c = a
+
+print(a == b)
+print(a is b)
+
+print(a == c)
+print(a is c)
+print(a == c)
+
+print(a == b and a is b)
+
+
+
+
