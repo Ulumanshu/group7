dfc1dd20a0a0524ed235abe3f23bae1de82452ff
Ulumanshu <karolis_kesminas@yahoo.com>
2021-06-12 10:03:58 +0300

    Add comparison operators

---
+++ comparison_operators.py
@@ -0,0 +1,11 @@
+# Comparison operators: ==, !=, <, >, <=, >=
+var_1 = "a"
+var2 = 'b'
+var3 = 5
+var4 = 4
+var5 = ''
+
+if var3 >= var4:
+    print(f'{var3} more or equals {var4}')
+else:
+    print(f'{var3} equals {var4}')
