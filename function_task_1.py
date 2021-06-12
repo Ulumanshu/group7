06622cfa7dfeceb3072616207fb5abb3be0d96a2
Ulumanshu <karolis_kesminas@yahoo.com>
2021-06-12 13:53:14 +0300

    Add task for functions with expl2 help tips

--- function_task_1.py
+++ function_task_1.py
@@ -1,9 +1,13 @@
 # 1. Nusiskaityti is failo data.txt kainu duomenis
 # 2. Susideti duomenis i dictionary
 # 3. Sukurti nauja objekta kuriam butu aishkiai parasyta kiek kg galima itemo nupirkti uz biudzeta.
-
+# split()
 budet_limit = 15 # eu
 
 with open('data.txt', 'rt') as f:
     file_data = f.read()
-    print(file_data)
+    split_lines = file_data.split('\n')
+    for line in split_lines:
+        if '-' in line:
+            print(line)
+
