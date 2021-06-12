a90df8a47025fbbcd9b542227ffe8c97cb76a832
Ulumanshu <karolis_kesminas@yahoo.com>
2021-06-12 09:52:58 +0300

    Add assigment operators

---
+++ opretaror_tryout.py
@@ -0,0 +1,6 @@
+# Assignment operators 	=, +=, -=, *=, **=, /=, //=, %=, &=, ^=, |=, <<=, >>=, |=
+sum_of_all = 0
+for e in range(100):
+    sum_of_all += e
+    print('ciklo elemento reiksme: ', e)
+    print('kintamojo i kuri sumuojama reiksme:', sum_of_all)
