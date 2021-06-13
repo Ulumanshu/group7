10b2deaaa327fab503f3ce566f974558808149eb
Ulumanshu <karolis_kesminas@yahoo.com>
2021-06-13 11:42:42 +0300

    Add list comprehension and multi dict keys

--- dict_get.py
+++ dict_get.py
@@ -1,5 +1,35 @@
-api_response = {'a': 1, 'b': 2, 'c': 3, 'weather': {'temp': None, 'sky': 'clear'}}
+api_response = {'a': 1, 'b': 2, 'c': 3, 'weather': {'temp': '', 'sky': 'clear'}}
 weather = api_response.get('weather', {}) or {}
 temp = weather.get('temp', 0) or 0
 sky = weather.get('sky', 0) or 0
 print(weather, temp, sky)
+
+
+# Tuples can be dictionary keys, sometimes it is very usefull if object properties are only unique together
+product_dict = {}
+tuple1 = (1234, 'Super Product XM-4450MX')
+
+product_dict[tuple1] = [
+    "Alpha Metal",
+    'Beta Electronics',
+    "Omega Nuclear",
+]
+print(product_dict)
+
+a = product_dict.get(tuple1)
+print(a)
+
+# Rasti pagal dali rakto
+for k, v in product_dict.items():
+    id_ = k[0]
+    name_ = k[1]
+    if id_ == 222:
+        pass
+    if name_ == 'asdf':
+        pass
+    print(id_, name_, v)
+
+# list comp
+tavo_elmentas = [v for k, v in product_dict.items() if k[0] == 1234]
+tavo_elmentas = len(tavo_elmentas) > 0 and tavo_elmentas[0] or False
+print(tavo_elmentas)
