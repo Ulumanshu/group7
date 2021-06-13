04f84dcd58663fce9e6450b30e3a142258de90cf
Ulumanshu <karolis_kesminas@yahoo.com>
2021-06-13 11:54:08 +0300

    Add set code from slides

---
+++ sets.py
@@ -0,0 +1,19 @@
+# Create a set
+animals = {"dog", "cat", "elephant"}
+
+# Add a new item
+animals.add("mouse")
+
+# Add several items at once
+animals.update(["bird", "horse"])
+print(animals)
+
+# Add the same item again
+animals.add("mouse")
+print(animals)
+
+# Remove item, Python will throw an error if it is not in the set
+animals.remove("cat")
+
+# Remove item, Python will NOT throw an error if it is not in the set
+animals.discard("cat")
--- tuples.py
+++ tuples.py
@@ -7,5 +7,5 @@ tuple_3 = "a", 2, "c", [1, 2, 3]
 
 # Accessing item (s) short
 tuple_4 = (1, 10.5, False, None, "string")
-print(tuple_4[2]) # Prints False
-print(tuple_4[1:3]) # Prints (10.5, False)
+print(tuple_4[2])  # Prints False
+print(tuple_4[1:3])  # Prints (10.5, False)
