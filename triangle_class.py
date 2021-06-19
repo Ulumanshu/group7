925569ce7788cd25d334ca68e9795b563c3ee01b
Ulumanshu <karolis_kesminas@yahoo.com>
2021-06-19 12:15:11 +0300

    Add class practice triangle plot

--- triangle_class.py
+++ triangle_class.py
@@ -1,4 +1,5 @@
 from math import pow, sqrt
+import matplotlib.pyplot as plt
 
 
 class Triangle:
@@ -6,6 +7,7 @@ class Triangle:
         self.point_1 = a
         self.point_2 = b
         self.point_3 = c
+        self.plot = plt
 
     def __repr__(self):
         return f"Trikampis A: {self.point_1}, B: {self.point_2}, C: {self.point_3}"
@@ -19,6 +21,18 @@ class Triangle:
         third_line = sqrt(pow(self.point_1[0] - self.point_3[0], 2) + pow(self.point_1[1] - self.point_3[1], 2))
         perimeter = first_line + second_line + third_line
         return perimeter
+
+    def plot_me(self):
+        l1_x_values = [self.point_1[0], self.point_2[0]]
+        l1_y_values = [self.point_1[1], self.point_2[1]]
+        self.plot.plot(l1_x_values, l1_y_values, linewidth=0.25, color='red')
+        l2_x_values = [self.point_2[0], self.point_3[0]]
+        l2_y_values = [self.point_2[1], self.point_3[1]]
+        self.plot.plot(l2_x_values, l2_y_values, linewidth=0.25, color='red')
+        l3_x_values = [self.point_3[0], self.point_1[0]]
+        l3_y_values = [self.point_3[1], self.point_1[1]]
+        self.plot.plot(l3_x_values, l3_y_values, linewidth=0.25, color='red')
+        self.plot.show()
 
 
 if __name__ == "__main__":
@@ -36,6 +50,7 @@ if __name__ == "__main__":
     print(new_triangles)
     for triangle in new_triangles:
         print(triangle.perimeter())
+        triangle.plot_me()
 
  # trikampis1 = Triangle()
 # print(trikampis1.point_1, trikampis1.point_2, trikampis1.point_3)
