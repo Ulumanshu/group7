a55977c3269faa5a8f185bc13911146dd09b232e
Ulumanshu <karolis_kesminas@yahoo.com>
2021-06-19 10:00:05 +0300

    Add class practice task 2

--- class_train.py
+++ class_train.py
@@ -10,13 +10,42 @@ class DasAuto:
         print(signalas)
 
 
-name_list = ['audi', 'vw', 'nissan']
-object_list = []
-for name in name_list:
-    instance = DasAuto(name)
-    print(instance)
-    object_list.append(instance)
+class Animal:
+    KINGDOM = "Animalia"  # class variable
 
-print(object_list)
-for object_ in object_list:
-    object_.papypsek_beep()
+    def __init__(self, name='John', age=2, tag='1z7sdgh'):
+        self.name = name  # set the default value for the name field of the Animal class object
+        self.age = age
+        self.__tag = tag
+
+    def print_details(self):  # method for printing the state of an object
+        print(f"Name: {self.name}, age: {self.age}.")
+
+    def set_tag(self, tag):
+        if len(tag) > 2:
+            self.__tag = tag
+        else:
+            print("Tsg must be longer than 2.")
+
+    def get_tag(self):
+        return self.__tag
+
+
+if __name__ == "__main__":
+    # DasAuto
+    name_list = ['audi', 'vw', 'nissan']
+    object_list = []
+    for name in name_list:
+        instance = DasAuto(name)
+        print(instance)
+        object_list.append(instance)
+    print(object_list)
+    for object_ in object_list:
+        object_.papypsek_beep()
+
+    # Animal
+    animal_objektas = Animal()
+    print(animal_objektas.KINGDOM, animal_objektas.name)
+    # Uzd 2. Sukurti 10 gyvunu su unikaliais parametrais
+    # Uzd 3. susideti sukurtus gyvunus i sarasa ir sekantis zingsnis atrinkti gyvunus jaunesnius nei 2 metai.
+
