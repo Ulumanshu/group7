1070b317fbbd1d09449015f57103df442b8ad03f
Ulumanshu <karolis_kesminas@yahoo.com>
2021-06-20 09:37:28 +0300

    Add class inheritance example

---
+++ class_inheritance.py
@@ -0,0 +1,16 @@
+class Human:
+    def __init__(self, name, height, weigth):
+        self.name = name
+        self.height = height
+        self.weigth = weigth
+
+
+class Programmer(Human):
+    def __init__(self, name, height, weigth, languages):
+        super().__init__(name, height, weigth)
+        self.languages = languages
+
+
+bob = Programmer("Bob", 180, 100, ["Python", "Java"])
+print(bob.name)  # Access to the name field inherited from the Human class
+print(bob.languages)
